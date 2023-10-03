343e934aeda3d0e098e70c9513d618b5661c608e
birdant <opbryant@gmail.com>
2023-10-03 00:39:13 -0400

    Donezo

--- ufund-ui/src/app/app.module.ts
+++ ufund-ui/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { NeedDetailComponent } from './need-detail/need-detail.component';
 import { NeedsComponent } from './needs/needs.component';
 import { MessagesComponent } from './messages/messages.component';
+import { NeedSearchComponent } from './need-search/need-search.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
@@ -23,7 +24,8 @@ import { HttpClientModule } from '@angular/common/http';
     DashboardComponent,
     NeedsComponent,
     NeedDetailComponent,
-    MessagesComponent
+    MessagesComponent,
+    NeedSearchComponent
   ],
   bootstrap: [ AppComponent ]
 })
