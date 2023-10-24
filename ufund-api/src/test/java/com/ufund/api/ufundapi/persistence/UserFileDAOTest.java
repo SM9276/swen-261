a2f8714ed915468fb0f4501280513584020d1d1e
Joseph DeSimone <jad3554@rit.edu>
2023-10-24 18:47:33 -0400

    Set up UserFileDAOTest constructor and imports

---
+++ ufund-api/src/test/java/com/ufund/api/ufundapi/persistence/UserFileDAOTest.java
@@ -0,0 +1,43 @@
+package com.ufund.api.ufundapi.persistence;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.io.File;
+import java.io.IOException;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ufund.api.ufundapi.model.User;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("Persistence-tier")
+public class UserFileDAOTest {
+    UserFileDAO userFileDAO;
+    User[] testUsers;
+    ObjectMapper mockObjectMapper;
+    
+    @Test
+    public void setupUserFileDAO() throws IOException {
+        mockObjectMapper = mock(ObjectMapper.class);
+        testUsers = new User[3];
+        testUsers[0] = new User(77, "Jud", "password1");
+        testUsers[1] = new User(78, "Zephyr", "password2");
+        testUsers[2] = new User(79, "Deric", "password3");
+
+        when(mockObjectMapper
+            .readValue(new File("ambiguous.txt"), User[].class))
+                .thenReturn(testUsers);
+        userFileDAO = new UserFileDAO("ambiguous.txt", mockObjectMapper);
+    }
+
+}
