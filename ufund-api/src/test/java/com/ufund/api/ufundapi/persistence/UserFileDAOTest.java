e9b0a9c038d4a528e6f474882f0ff065622b8366
Joseph DeSimone <jad3554@rit.edu>
2023-10-24 22:50:01 -0400

    Implemented the rest of the unit tests based on the template for the first time

--- ufund-api/src/test/java/com/ufund/api/ufundapi/persistence/UserFileDAOTest.java
+++ ufund-api/src/test/java/com/ufund/api/ufundapi/persistence/UserFileDAOTest.java
@@ -30,7 +30,7 @@ public class UserFileDAOTest {
     public void setupUserFileDAO() throws IOException {
         mockObjectMapper = mock(ObjectMapper.class);
         testUsers = new User[3];
-        testUsers[0] = new User(77, "Jud", "password1");
+        testUsers[0] = new User(77, "Joseph", "password1");
         testUsers[1] = new User(78, "Zephyr", "password2");
         testUsers[2] = new User(79, "Deric", "password3");
 
@@ -50,4 +50,135 @@ public class UserFileDAOTest {
         }
     }
 
+    @Test
+    public void testFindUsers() {
+        // Invoke
+        User[] users = userFileDAO.findUsers("ph");
+
+        // Analyze
+        assertEquals(users.length,2);
+        assertEquals(users[0],testUsers[1]);
+        assertEquals(users[1],testUsers[2]);
+    }
+
+    @Test
+    public void testGetUser() {
+        // Invoke
+        User user = userFileDAO.getUser(77);
+
+        // Analzye
+        assertEquals(user,testUsers[0]);
+    }
+
+    @Test
+    public void testDeleteHero() {
+        // Invoke
+        boolean result = assertDoesNotThrow(() -> userFileDAO.deleteUser(77),
+                            "Unexpected exception thrown");
+
+        // Analzye
+        assertEquals(result,true);
+        // We check the internal tree map size against the length
+        // of the test heroes array - 1 (because of the delete)
+        // Because heroes attribute of UserFileDAO is package private
+        // we can access it directly
+        assertEquals(userFileDAO.users.size(),testUsers.length-1);
+    }
+
+    @Test
+    public void testCreateUser() {
+        // Setup
+        User user = new User(4, "Jared", "OPM");
+
+        // Invoke
+        User result = assertDoesNotThrow(() -> userFileDAO.createUser(user),
+                                "Unexpected exception thrown");
+
+        // Analyze
+        assertNotNull(result);
+        User actual = userFileDAO.getUser(user.getId());
+        assertEquals(actual.getId(),user.getId());
+        assertEquals(actual.getUsername(),user.getUsername());
+    }
+
+    @Test
+    public void testUpdateHero() {
+        // Setup
+        User user = new User(6,"Galactic Jud", "JedMode");
+
+        // Invoke
+        User result = assertDoesNotThrow(() -> userFileDAO.updateUser(user),
+                                "Unexpected exception thrown");
+
+        // Analyze
+        assertNotNull(result);
+        User actual = userFileDAO.getUser(user.getId());
+        assertEquals(actual,user);
+    }
+
+    @Test
+    public void testSaveException() throws IOException{
+        doThrow(new IOException())
+            .when(mockObjectMapper)
+                .writeValue(any(File.class),any(User[].class));
+
+        User user = new User(102,"Mr. Smiley", "ORV");
+
+        assertThrows(IOException.class,
+                        () -> userFileDAO.createUser(user),
+                        "IOException not thrown");
+    }
+
+    @Test
+    public void testGetUserNotFound() {
+        // Invoke
+        User user = userFileDAO.getUser(78);
+
+        // Analyze
+        assertEquals(user,null);
+    }
+
+    @Test
+    public void testDeleteUserNotFound() {
+        // Invoke
+        boolean result = assertDoesNotThrow(() -> userFileDAO.deleteUser(78),
+                                                "Unexpected exception thrown");
+
+        // Analyze
+        assertEquals(result,false);
+        assertEquals(userFileDAO.users.size(),testUsers.length);
+    }
+
+    @Test
+    public void testUpdateHeroNotFound() {
+        // Setup
+        User user = new User(78,"Goku", "Gohan's Birthday");
+
+        // Invoke
+        User result = assertDoesNotThrow(() -> userFileDAO.updateUser(user),
+                                                "Unexpected exception thrown");
+
+        // Analyze
+        assertNull(result);
+    }
+
+    @Test
+    public void testConstructorException() throws IOException {
+        // Setup
+        ObjectMapper mockObjectMapper = mock(ObjectMapper.class);
+        // We want to simulate with a Mock Object Mapper that an
+        // exception was raised during JSON object deseerialization
+        // into Java objects
+        // When the Mock Object Mapper readValue method is called
+        // from the userFileDAO load method, an IOException is
+        // raised
+        doThrow(new IOException())
+            .when(mockObjectMapper)
+                .readValue(new File("doesnt_matter.txt"),User[].class);
+
+        // Invoke & Analyze
+        assertThrows(IOException.class,
+                        () -> new UserFileDAO("doesnt_matter.txt",mockObjectMapper),
+                        "IOException not thrown");
+    }
 }
