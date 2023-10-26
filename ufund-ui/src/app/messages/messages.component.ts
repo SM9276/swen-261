0a0250a7c7584dc22ebad774fcabe4063bf8e08f
birdant <opbryant@gmail.com>
2023-10-26 06:35:18 -0400

    small fixes

--- ufund-ui/src/app/messages/messages.component.ts
+++ ufund-ui/src/app/messages/messages.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { MessageService } from '../message.service';
+import { AppComponent } from '../app.component';
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
   styleUrls: ['./messages.component.css']
 })
 export class MessagesComponent {
-  constructor(public messageService: MessageService) {}
+  constructor(public messageService: MessageService, private appComponent: AppComponent) {}
+
+  getUsername(): String {
+    return this.appComponent.getUsername();
+  }
 }
 
