539035b19605ebd201fa5ee64c5d00cadfcf3a2d
sm9276 <sm9276@rit.edu>
2023-11-14 04:11:37 -0500

    UI router refactored

--- ufund-ui/src/app/need-detail/need-detail.component.ts
+++ ufund-ui/src/app/need-detail/need-detail.component.ts
@@ -1,9 +1,8 @@
-
 import { Need } from '../need';
 import { Component, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
-
+import { Router, NavigationExtras } from '@angular/router';
 import { UserService } from '../user.service';
 
 import { AppComponent } from '../app.component';
@@ -29,6 +28,7 @@ export class NeedDetailComponent {
     private userService: UserService,
 
     private appComponent: AppComponent,
+    private router :Router,
   ) {}
   ngOnInit(): void {
     this.getNeed();
@@ -53,34 +53,39 @@ export class NeedDetailComponent {
   }
 
   addToBasket(): void{
-    if (this.need) {
-      const username: String = (this.appComponent.login).trim();
-      const basket = this.userService.getFundingBasket(username);
-      const id = Number(this.route.snapshot.paramMap.get('id'));
-      basket.subscribe((fundingBasket) => {this.fundingBaskets.push(fundingBasket);
-      console.log(fundingBasket);
-      this.needService.getNeed(id).subscribe(need => this.need = need);
-      console.log(this.need);
-      console.log(this.need.quantity);
-      console.log(this.fundingBaskets[0].needs.length);
-      console.log(this.needInBasket());
-      if(this.fundingBaskets[0].needs.length == 0){
-        this.fundingBaskets[0].needs.push(this.need);
-        console.log(this.fundingBaskets[0].needs);
+    if(this.appComponent.getUsername()){
+      if (this.need) {
+        const username: String = (this.appComponent.login).trim();
+        const basket = this.userService.getFundingBasket(username);
+        const id = Number(this.route.snapshot.paramMap.get('id'));
+        basket.subscribe((fundingBasket) => {this.fundingBaskets.push(fundingBasket);
+        console.log(fundingBasket);
+        this.needService.getNeed(id).subscribe(need => this.need = need);
+        console.log(this.need);
+        console.log(this.need.quantity);
+        console.log(this.fundingBaskets[0].needs.length);
+        console.log(this.needInBasket());
+        if(this.fundingBaskets[0].needs.length == 0){
+          this.fundingBaskets[0].needs.push(this.need);
+          console.log(this.fundingBaskets[0].needs);
+        }
+        else if(this.needInBasket()){
+          this.fundingBaskets[0].needs.forEach((need) => {
+            if(this.need.id == need.id){
+              need.quantity = Number(need.quantity) + Number(this.need.quantity);
+            }
+          });
+        }
+        else {
+          this.fundingBaskets[0].needs.push(this.need);
+          console.log(this.fundingBaskets[0].needs);
+        }
+        this.userService.updateFundingBasket(this.fundingBaskets[0]).subscribe(() => this.goBack());
+      });
       }
-      else if(this.needInBasket()){
-        this.fundingBaskets[0].needs.forEach((need) => {
-          if(this.need.id == need.id){
-            need.quantity = Number(need.quantity) + Number(this.need.quantity);
-          }
-        });
-      }
-      else {
-        this.fundingBaskets[0].needs.push(this.need);
-        console.log(this.fundingBaskets[0].needs);
-      }
-      this.userService.updateFundingBasket(this.fundingBaskets[0]).subscribe(() => this.goBack());
-    });
+    }else{
+      this.router.navigate(['login']);
+      window.alert('Please Login before adding to funding basket');
     }
   }
   getUsername(): String {
