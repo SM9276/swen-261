c5179d94384692a43cc6c27279eb1f6ca010a436
Jay <jay1018lee@gmail.com>
2023-11-13 21:06:21 -0500

    html/css

--- ufund-ui/src/app/app.component.ts
+++ ufund-ui/src/app/app.component.ts
@@ -12,13 +12,11 @@ export class AppComponent {
   title = 'Orphan Helpers';
   login: string = "";
   canDisplay: string = "";
-  logStatus: boolean = false;
   constructor(private cookie: CookieService, private authenticationService: AuthenticationService, private router: Router){
   }
 
   ngOnInit(): void{
     this.login = this.cookie.get("UserId");
-    this.isLoggedIn();
     console.log("Get cookie " + this.login);
     console.log("Get cookie " + this.canDisplay);
   }
@@ -28,20 +26,7 @@ export class AppComponent {
     this.cookie.set("UserId",this.login);
     console.log("Set cookie " + this.login);
   }
-  isLoggedIn() {
-    const loggedIn: string = this.getUsername();
   
-    if (loggedIn !== "") {
-      console.log(this.getUsername)
-      console.log("Logged In");
-      this.logStatus = true;
-      return true;
-    } else {
-      console.log("Not Logged In");
-      this.logStatus = false;
-      return false;
-    }
-  }
   public logout() {
     this.login = "";
     this.cookie.delete("UserId");
